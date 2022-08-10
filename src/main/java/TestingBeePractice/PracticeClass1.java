bc9c816a25c248d32606dc8b03aa0851f36b75ea
shahanalata <shahanalata84@gmail.com>
2022-08-10 14:53:58 -0400

    update new

---
+++ src/main/java/TestingBeePractice/PracticeClass1.java
@@ -0,0 +1,13 @@
+package TestingBeePractice;
+
+public class PracticeClass1 {
+int a;
+int b;
+int c;
+int d;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
